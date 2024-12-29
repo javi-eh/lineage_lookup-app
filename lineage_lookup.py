bd74ec33c0a946c5e283f02302588f0ae526a541
javi-eh <jespinozaherrera@ucsd.edu>
2024-12-29 12:15:30 -0800

    Update lineage_lookup.py

--- lineage_lookup.py
+++ lineage_lookup.py
@@ -10,12 +10,20 @@ file_name = 'proteomes_proteome_type_1_2024_12_19.tsv.gz'
 def load_data(file_name):
     with gzip.open(file_name, 'rb') as f:
         df = pd.read_csv(f, sep='\t')
-
     df = df[df['Taxonomic lineage'].apply(lambda x: isinstance(x, str))]
     df['Taxonomic lineage'] = df['Taxonomic lineage'].apply(lambda x: [item.strip() for item in x.split(",")])
     return df
 
 df = load_data(file_name)
+
+available_taxa = set()
+for lineage in df['Taxonomic lineage']:
+    available_taxa.update(lineage)
+available_taxa = sorted(available_taxa)
+
+def filter_taxa(query):
+    query = query.lower()
+    return [taxon for taxon in available_taxa if query in taxon.lower()]
 
 def find_organisms_and_sublevels(df, taxon):
     taxon = taxon.lower()
@@ -35,11 +43,20 @@ def find_organisms_and_sublevels(df, taxon):
 st.title("Lineage lookup")
 st.write("Search for closely related organisms by entering a taxon below.")
 
-selected_taxon = st.text_input("Enter the taxon you wish to explore (e.g., Euteleostomi, Fungi, Streptophyta):") 
+search_query = st.text_input("Type a taxonomic term:") 
+
+if search_query:
+    matching_taxa = filter_taxa(search_query)
+    if matching_taxa:
+        selected_taxon = st.selectbox("Select from matching taxa:", matching_taxa)
+    else:
+        st.write("No matching taxa found.")
+        selected_taxon = None
+else:
+    selected_taxon = None
 
 if selected_taxon:
     result = find_organisms_and_sublevels(df, selected_taxon)
-    
     if result:
         st.write(f"### Organisms under '{selected_taxon}':")
         for sublevel, organisms in result.items():
